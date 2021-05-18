c64db70be57f551e9201c0f627b9b3d642ec6247
Clonkk <sidger@zoho.com>
2021-05-18 18:15:10 +0200

    commented unused code

--- src/tocgen.nim
+++ src/tocgen.nim
@@ -2,30 +2,76 @@ import os
 import strformat
 import algorithm
 
-const path_to_root = "{{path_to_root}}"
-var excludedFile: seq[string]
-
-proc hasContent(folder: string): bool =
-  for kind, path in walkdir(folder):
-    if kind == pcFile:
-      let (dir, name, ext) = path.splitfile()
-      # TODO fix this
-      if ext == ".nim" and name in ["index", "contributors"]:
-        debugEcho &"    hasContent=> {folder}, {path} => True"
-        return true
-  return false
-
 type
   Entry = object
     title: string
-    name: string
+    path: string
     levels: seq[int]
     isSection: bool
+#############################################################################
+## This section is only useful to create a seq of Entries from books files ##
+## After consideration, it is easier to have an API to construct entries   ##
+#############################################################################
+# var entries : seq[Entry]
+# var excludedFile: seq[string]
+#
+# proc makeChapter(title, name: string, levels: varargs[int]) =
+#   entries.add Entry(title: title, path: name, levels: @levels, isSection: false)
+#
+# proc makeSection(title, name: string, levels: varargs[int]) =
+#   entries.add Entry(title: title, path: name, levels: @levels, isSection: true)
+#
+# proc hasContent(folder: string): bool =
+#   for kind, path in walkdir(folder):
+#     if kind == pcFile:
+#       let (dir, name, ext) = path.splitfile()
+#       # TODO fix this
+#       if ext == ".nim" and name in ["index", "contributors"]:
+#         debugEcho &"    hasContent=> {folder}, {path} => True"
+#         return true
+#   return false
+#
+# proc populateEntries(rootdir: string, levels: varargs[int], addIndex: bool = true) =
+#   once:
+#     makeChapter(rootdir, "index", 1)
+#
+#   debugEcho "=> populateEntries : ", rootdir
+#   var levels : seq[int] = @levels
+#   if addIndex:
+#     levels.add 1
+#
+#   for kind, path in walkDir(rootdir):
+#     var (dir, name, ext) = path.splitfile()
+#     if kind == pcFile:
+#       # Add file as incremented numbers
+#       if ext == ".nim" and name notin ["index"] and path notin excludedFile:
+#         makeChapter(name, dir/name, levels)
+#         inc(levels[^1])
+#
+#     elif kind == pcDir:
+#       debugEcho "  ", kind, " => path=", path
+#       if hasContent(path):
+#         inc(levels[^1])
+#         if fileExists(path/"index.nim"):
+#           debugEcho &"    Found index.nim... => ({dir}, {name}, {ext})"
+#           makeSection(name, path/"index", levels)
+#         else:
+#           var counter = 0
+#           for f in walkFiles(path/"*.nim"):
+#             doAssert(counter == 0, "Error folder that do not contain index.nim can only contains a single Nim file")
+#             let (dir, name, ext) = f.splitfile()
+#             debugEcho &"    Walking files... => {f} => ({dir}, {name}, {ext})"
+#             makeSection(name, dir/name, levels)
+#             excludedFile.add f
+#             inc(counter)
+#         populateEntries(path, levels, true)
+#       else:
+#         populateEntries(path, levels, false)
+####################
+## End of section ##
+####################
 
-var entries : seq[Entry]
-
-proc makeChapter(title, name: string, levels: varargs[int]) : string =
-  entries.add Entry(title: title, name: name, levels: @levels, isSection: false)
+const path_to_root = "{{path_to_root}}"
 
 proc closeSection() : string =
   # First chapter has no opened section so close should do nothing
@@ -63,49 +109,10 @@ proc makeSectionImpl(title, name: string, levels: varargs[int]) : string =
   result.add makeChapterImpl(title, name, levels)
   result.add openSection()
 
-proc makeSection(title, name: string, levels: varargs[int]) : string =
-  entries.add Entry(title: title, name: name, levels: @levels, isSection: true)
-
-proc gentoc(rootdir: string, levels: varargs[int], addIndex: bool = true) : string =
-  debugEcho "=> gentoc : ", rootdir
-  var levels : seq[int] = @levels
-  if addIndex:
-    levels.add 1
-
-  for kind, path in walkDir(rootdir):
-    var (dir, name, ext) = path.splitfile()
-    if kind == pcFile:
-      # Add file as incremented numbers
-      if ext == ".nim" and name notin ["index"] and path notin excludedFile:
-        result.add makeChapter(name, dir/name, levels)
-        inc(levels[^1])
-
-    elif kind == pcDir:
-      debugEcho "  ", kind, " => path=", path
-      if hasContent(path):
-        inc(levels[^1])
-        if fileExists(path/"index.nim"):
-          debugEcho &"    Found index.nim... => ({dir}, {name}, {ext})"
-          result.add makeSection(name, path/"index", levels)
-        else:
-          var counter = 0
-          for f in walkFiles(path/"*.nim"):
-            doAssert(counter == 0, "Error folder that do not contain index.nim can only contains a single Nim file")
-            let (dir, name, ext) = f.splitfile()
-            debugEcho &"    Walking files... => {f} => ({dir}, {name}, {ext})"
-            result.add makeSection(name, dir/name, levels)
-            excludedFile.add f
-            inc(counter)
-        result.add gentoc(path, levels, true)
-      else:
-        result.add gentoc(path, levels, false)
-
-
 proc openGenToc(rootdir: string) : string =
   result.add """
 <ol class="chapter">
 """
-  result.add makeChapter(rootdir, "index", 1)
 
 proc closeGenToc() : string =
   # Close last opened section that will not be closed anywhere
@@ -143,23 +150,25 @@ proc entrycmp(x, y: Entry): int=
 proc write_gentoc(rootdir, filename: string) =
   var r: string
   r.add openGenToc(rootdir)
-  discard gentoc(rootdir)
+  # You can use gentoc to parse rootdir foler and generate an arborescence
+  # It might be simpler to just expose an API to construct the seq[Entry]
+  # populateEntries(rootdir)
 
-  var entries = @[Entry(title: "Introduction", name: "index", levels: @[1], isSection: false),
-                  Entry(title: "Basics", name: "books/basics/index", levels: @[2], isSection: true),
-                  Entry(title: "Models", name: "books/basics/models", levels: @[2, 2], isSection: false),
-                  Entry(title: "Data Manipulation", name: "books/basics/data_manipulation", levels: @[2, 1], isSection: false),
-                  Entry(title: "Plotting", name: "books/basics/plotting", levels: @[2, 3], isSection: false),
-                  Entry(title: "Contributors", name: "books/misc/but/very/far/contributors", levels: @[3], isSection: true)
+  var entries = @[Entry(title: "Introduction", path: "index", levels: @[1], isSection: false),
+                  Entry(title: "Basics", path: "basics/index", levels: @[2], isSection: true),
+                  Entry(title: "Models", path: "basics/models", levels: @[2, 2], isSection: false),
+                  Entry(title: "Data Manipulation", path: "basics/data_manipulation", levels: @[2, 1], isSection: false),
+                  Entry(title: "Plotting", path: "basics/plotting", levels: @[2, 3], isSection: false),
+                  Entry(title: "Contributors", path: "misc/but/very/far/contributors", levels: @[3], isSection: true)
                   ]
   var sortedentries = entries.sorted(entrycmp)
   echo sortedentries
   for e in sortedentries:
-    echo "    ", e.title, " ==> ", e.name, " ==> ", e.isSection
+    echo "    ", e.title, " ==> ", e.path, " ==> ", e.isSection
     if e.isSection:
-      r.add makeSectionImpl(e.title, e.name, e.levels)
+      r.add makeSectionImpl(e.title, e.path, e.levels)
     else:
-      r.add makeChapterImpl(e.title, e.name, e.levels)
+      r.add makeChapterImpl(e.title, e.path, e.levels)
 
   r.add closeGenToc()
   # TODO use rootdir / filename
@@ -168,7 +177,6 @@ proc write_gentoc(rootdir, filename: string) =
   f.write(r)
 
 when isMainModule:
-  # write_gentoc("books", "tocgen.mustache.html")
   write_gentoc("books", "books/toc.mustache")
   copyFile("books/toc.mustache", "tocgen.mustache.html")
 
