85044978255544c5fe273bdf5fe1e5a0a72bb11b
Clonkk <sidger@zoho.com>
2021-05-18 17:45:37 +0200

    ordering entries using levels now works

--- src/tocgen.nim
+++ src/tocgen.nim
@@ -1,5 +1,6 @@
 import os
 import strformat
+import algorithm
 
 const path_to_root = "{{path_to_root}}"
 var excludedFile: seq[string]
@@ -14,20 +15,17 @@ proc hasContent(folder: string): bool =
         return true
   return false
 
-proc makeChapter(title, name: string, levels: varargs[int]) : string =
-  debugEcho &"    makeChapter => {title} : {name} => {levels}"
-  var chapNumber  : string
-  for i in levels:
-    chapNumber.add $i
-    chapNumber.add "."
+type
+  Entry = object
+    title: string
+    name: string
+    levels: seq[int]
+    isSection: bool
 
-  result.add &"""
-  <li class="chapter-item expanded ">
-    <a href="{path_to_root}{name}.html" tabindex="0">
-      <strong aria-hidden="true">{chapNumber}</strong> {title}
-    </a>
-  </li>
-"""
+var entries : seq[Entry]
+
+proc makeChapter(title, name: string, levels: varargs[int]) : string =
+  entries.add Entry(title: title, name: name, levels: @levels, isSection: false)
 
 proc closeSection() : string =
   # First chapter has no opened section so close should do nothing
@@ -44,6 +42,29 @@ proc openSection() : string =
   <li>
   <ol class="section">
 """
+
+proc makeChapterImpl(title, name: string, levels: varargs[int]) : string =
+  debugEcho &"    makeChapter => {title} : {name} => {levels}"
+  var chapNumber  : string
+  for i in levels:
+    chapNumber.add $i
+    chapNumber.add "."
+
+  result.add &"""
+  <li class="chapter-item expanded ">
+    <a href="{path_to_root}{name}.html" tabindex="0">
+      <strong aria-hidden="true">{chapNumber}</strong> {title}
+    </a>
+  </li>
+"""
+
+proc makeSectionImpl(title, name: string, levels: varargs[int]) : string =
+  result.add closeSection()
+  result.add makeChapterImpl(title, name, levels)
+  result.add openSection()
+
+proc makeSection(title, name: string, levels: varargs[int]) : string =
+  entries.add Entry(title: title, name: name, levels: @levels, isSection: true)
 
 proc gentoc(rootdir: string, levels: varargs[int], addIndex: bool = true) : string =
   debugEcho "=> gentoc : ", rootdir
@@ -65,18 +86,14 @@ proc gentoc(rootdir: string, levels: varargs[int], addIndex: bool = true) : stri
         inc(levels[^1])
         if fileExists(path/"index.nim"):
           debugEcho &"    Found index.nim... => ({dir}, {name}, {ext})"
-          result.add closeSection()
-          result.add makeChapter(name, path/"index", levels)
-          result.add openSection()
+          result.add makeSection(name, path/"index", levels)
         else:
           var counter = 0
           for f in walkFiles(path/"*.nim"):
             doAssert(counter == 0, "Error folder that do not contain index.nim can only contains a single Nim file")
             let (dir, name, ext) = f.splitfile()
             debugEcho &"    Walking files... => {f} => ({dir}, {name}, {ext})"
-            result.add closeSection()
-            result.add makeChapter(name, dir/name, levels)
-            result.add openSection()
+            result.add makeSection(name, dir/name, levels)
             excludedFile.add f
             inc(counter)
         result.add gentoc(path, levels, true)
@@ -97,10 +114,47 @@ proc closeGenToc() : string =
 </ol>
 """
 
+proc intcmp(x, y: int): int =
+  if x < y:
+    return -1
+  elif x == y:
+    return 0
+  else:
+    return 1
+
+proc mycmp(x, y: seq[int]): int =
+  var idx = 0
+  var r = intcmp(x[idx], y[idx])
+  while r == 0:
+    inc(idx)
+    if idx >= x.len() and idx < y.len():
+      return -1
+    if idx >= y.len() and idx < x.len():
+      return 1
+    else:
+      r = intcmp(x[idx], y[idx])
+  return r
+
+proc entrycmp(x, y: Entry): int=
+  return mycmp(x.levels, y.levels)
+
+proc sortEntries() : seq[Entry] =
+  result = entries.sorted(entrycmp)
+
 proc write_gentoc(rootdir, filename: string) =
   var r: string
   r.add openGenToc(rootdir)
-  r.add gentoc(rootdir)
+  discard gentoc(rootdir)
+
+  var sortedentries = sortEntries()
+  echo sortedentries
+  for e in sortedentries:
+    echo "    ", e.title, " ==> ", e.name, " ==> ", e.isSection
+    if e.isSection:
+      r.add makeSectionImpl(e.title, e.name, e.levels)
+    else:
+      r.add makeChapterImpl(e.title, e.name, e.levels)
+
   r.add closeGenToc()
   # TODO use rootdir / filename
   let f = open(filename, fmWrite)
