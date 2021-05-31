254e86a699aad9308894e64416a035d1a1aa8ef4
Pietro Peterlongo <pietro.peterlongo@gmail.com>
2021-05-31 21:36:45 +0200

    add example toc structure

--- genbook.nim
+++ genbook.nim
@@ -6,6 +6,11 @@ var book = newBookFromToc("nimibook", "book"):
     entry("Toc DSL", "tocdsl")
     draft("Configuration", "configuration.md")
     entry("Tasks", "tasks")
+  section("Example toc structure", "tocexample/index.md"):
+    section("Nested section", "nested.md"):
+      entry("Entry in nested section", "nested_entry.md")
+    entry("Back to parent section", "back_to_parent.md")
+    draft("Draft chapter", "draft.md")
 
 book.git_repository_url = "https://github.com/pietroppeter/nimibook"
 nbBookTasks
