09c4f6ed88872f42f03bc03c8dddfb928f01340d
Clonkk <sidger@zoho.com>
2021-06-01 14:28:53 +0200

    added examples folder for a out of repo, example

---
+++ examples/book/book_1.nim
@@ -0,0 +1,11 @@
+import nimib, nimibook
+
+nbInit
+
+nbUseNimibook # overrides nimib defaults with nimibooks and loads book configuration
+
+nbText: """ ## This is a title
+This is a sentence in a page
+"""
+
+nbSave
---
+++ examples/examplebook.nim
@@ -0,0 +1,7 @@
+import nimibook, os
+
+var book = newBookFromToc("Dummy Book", "book"):
+  section("Dummy", "index"):
+    entry("Simple example", "book_1")
+
+nbBookTasks
