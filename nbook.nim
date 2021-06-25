d6de6c33119d6fa8ece64e8eb1c75c50e054a823
Pietro Peterlongo <pietro.peterlongo@gmail.com>
2021-06-25 17:55:20 +0200

    need to add also in toc

--- nbook.nim
+++ nbook.nim
@@ -6,6 +6,7 @@ var book = newBookFromToc("nimibook", "book"):
     entry("Toc DSL", "tocdsl")
     entry("Configuration", "configuration")
     entry("Tasks", "tasks")
+  section("test", "test/add_and_remove")
   section("Example toc structure", "tocexample/index.md"):
     section("Nested section", "nested.md"):
       entry("Entry in nested section", "nested_entry.md")
