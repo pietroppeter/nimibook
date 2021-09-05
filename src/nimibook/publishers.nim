241a568a4e7f947d9f42d702bfb915a13ba2bb3f
Pietro Peterlongo <pietro.peterlongo@gmail.com>
2021-09-05 19:10:26 +0200

    fixes required to make it run with latest nimib (breaks mdPublish)

--- src/nimibook/publishers.nim
+++ src/nimibook/publishers.nim
@@ -10,20 +10,18 @@ proc nimPublish*(entry: Entry) =
     # So here's a little trick to propagate the info
     # See comment in docs.nim useNimibook() proc
     nimibSrcDir = getEnv("nimibSrcDir")
-    nimibOutDir = nimibSrcDir.parentDir() / "docs"
-    args = ["r", "-d:release", "-f", &"-d:nimibSrcDir={nimibSrcDir}", &"-d:nimibOutDir={nimibOutDir}", "--verbosity:0", "--hints:off", entry.path]
+    nimibHomeDir = nimibSrcDir.parentDir() / "docs"
+    args = ["r", "-d:release", "-f", &"-d:nimibSrcDir={nimibSrcDir}", &"-d:nimibHomeDir={nimibHomeDir}", "--verbosity:0", "--hints:off", entry.path]
   debugEcho "[Executing] ", cmd, " ", args.join(" ")
   if execShellCmd(cmd & " " & args.join(" ")) != 0:
     quit(1)
 
 proc mdPublish*(entry: Entry) =
   nbInit
-  nbDoc.filename = (nbThisDir / ("../../" & entry.path).RelativeDir).string
+  nbDoc.filename = (nb.thisDir / ("../../" & entry.path).RelativeDir).string
   nbDoc.useNimibook
-  withDir(nbHomeDir / "..".RelativeDir):
-    nbText entry.path.readFile
+  nbText entry.path.readFile
   nbSave
-  setCurrentDir nbInitDir # reset current directory
 
 proc publish*(entry: Entry) =
   let splitted = entry.path.splitFile()
@@ -37,6 +35,7 @@ proc publish*(entry: Entry) =
 proc publish*(book: Book) =
   dump book
   for entry in book.toc.entries:
+    echo "[nimibook] publish entry: ", entry.path
     entry.publish()
   cleanjson book
   check book
