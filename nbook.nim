7b2ca977410cf0180fd60f4d9ed1932a2a2e843c
Pietro Peterlongo <pietro.peterlongo@gmail.com>
2023-02-19 17:33:31 +0100

    change toc structure + latex in content chapter

--- nbook.nim
+++ nbook.nim
@@ -1,12 +1,11 @@
 import nimibook
 
 var book = initBookWithToc:
-  section("Introduction", "index"): # .nim extension is optional
-    entry("Content", "content")
-    entry("Toc DSL", "tocdsl")
-    entry("Configuration", "configuration")
-    entry("Nimble tasks", "tasks")
-    entry("Latex", "latex")
+  entry("Introduction", "index", numbered = false) # .nim extension is optional
+  entry("Content", "content")
+  entry("Table of Contents", "toc")
+  entry("Configuration", "configuration")
+  entry("Commands", "cli")
   section("Example toc structure", "tocexample/index.md"):
     section("Nested section", "nested.md"):
       entry("Entry in nested section", "nested_entry.md")
