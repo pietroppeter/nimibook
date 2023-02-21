ef99b6901a776d3f64ad10be62cc2faffef4fb26
Pietro Peterlongo <pietro.peterlongo@gmail.com>
2023-02-21 07:41:29 +0100

    fix mention section implictly adds relative path

--- book/toc.nim
+++ book/toc.nim
@@ -18,6 +18,8 @@ From the above example you can see that:
   - source path is relative to book source folder
   - source extension defaults to `.nim`, to use a markdown source you need to explict mark it as `.md`
   - chapters that start a section are created with `section` and start a new block
+    - **note** that the path of section folder is added implicitly to all childrens (entries or sections)
+      of a section (e.g. `nested.md` is inside `tocexample` folder)
   - draft chapters (placeholders in the ToC without source) are created with `draft` 
 
 In the future nimibook will support the same `SUMMARY.md` mechanism as in mdbook.
