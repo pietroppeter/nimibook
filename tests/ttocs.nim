480d23b792c87bdb4cec9b4975becf8a33ff37d2
Pietro Peterlongo <pietro.peterlongo@gmail.com>
2023-02-24 12:28:07 +0100

    add test for toc render bug

--- tests/ttocs.nim
+++ tests/ttocs.nim
@@ -1,5 +1,6 @@
 import unittest
-import nimibook / [tocs, entries, types]
+import std / strutils
+import nimibook / [tocs, entries, types, renders]
 
 test "toc dsl":
   var book: Book
@@ -21,3 +22,15 @@ test "toc dsl":
   check book.toc.entries[0].url == "index.html"
   check book.toc.entries[1].url == "part1/index.html"
   check book.toc.entries[2].url == "part1/important.html"
+
+test "toc render": # issue 73
+  let myToc = initToc:
+    entry("Should Be 1.", "book/bla.nim")
+    section("Should be 2", "book/internals.nim"):    
+      section("Should be 2.1", "internals/adders.nim"):
+        entry("Should be 2.1.1", "adders/one_adder.nim")
+
+    entry("Should be 3.", "CONTRIBUTING.md")
+  let renderedToc = myToc.render
+  check renderedToc.count("<ol") == renderedToc.count("</ol>")
+  check renderedToc.count("<li") == renderedToc.count("</li>")
